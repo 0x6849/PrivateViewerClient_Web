f1770d78031a81eba75b9e3519648617aba547d7
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-04-22 19:05:20 +0200

    New video player

--- logic.js
+++ logic.js
@@ -2,18 +2,27 @@ const file = document.querySelector("#file");
 const vid = document.querySelector("video");
 const txtSpeed = document.querySelector("#txtSpeed");
 const speed = document.querySelector("#speed");
+const btnPlay = document.querySelector("#btnPlay");
+const btnPause = document.querySelector("#btnPause");
+const btnStop = document.querySelector("#btnStop");
+const timestampLabel = document.querySelector("#timestamp");
+const vidProgress = document.querySelector("#vidProgress");
+const txtVol = document.querySelector("#txtVol");
+const volume = document.querySelector("#volume");
 var socket;
-var programPlayed = false;
-var programPaused = false;
-var programSeeked = false;
 var lastState;
+var roomName = prompt("Enter room id", "r1");
 
 function init() {
     file.addEventListener("change", fileSelected);
-    vid.addEventListener("seeked", videoSeeked);
-    vid.addEventListener("play", videoPlayed);
-    vid.addEventListener("pause", videoPaused);
-    speed.addEventListener("input", changeSpeed);
+    speed.addEventListener("change", changeSpeed);
+    speed.addEventListener("input", inputSpeed);
+    volume.addEventListener("input", changeVolume);
+    vidProgress.addEventListener("change", videoSeeked);
+    btnPlay.addEventListener("click", videoPlayed);
+    btnPause.addEventListener("click", videoPaused);
+    btnStop.addEventListener("click", videoStopped);
+    video.addEventListener("timeupdate", updateSlider);
     initSocket();
 }
 
@@ -23,7 +32,7 @@ function initSocket() {
     socket.addEventListener("open", () => {
         socket.send(JSON.stringify({
             "action": "join",
-            "roomID": prompt("Enter room id", "r1"),
+            "roomID": roomName,
             "name": "webclient_" + Math.round(new Date() / 1000)
         }));
     });
@@ -70,54 +79,99 @@ function updateVideo(newState) {
     lastState = newState;
 }
 
-function videoSeeked() {
-    if (programSeeked) {
-        programSeeked = false;
-    } else {
-        lastState = undefined;
-        if (socket.readyState == WebSocket.OPEN) {
-            socket.send(JSON.stringify({
-                "timeStamp": vid.currentTime,
-                "action": "change"
-            }));
-        }
-    }
-}
-
-function videoPlayed() {
-    if (programPlayed) {
-        programPlayed = false;
-    } else {
-        lastState = undefined;
+function videoSeeked(e) {
+    lastState = undefined;
+    if (socket.readyState == WebSocket.OPEN) {
         socket.send(JSON.stringify({
-            "paused": false,
+            "timeStamp": parseFloat(vidProgress.value) / 100.0 * vid.duration,
             "action": "change"
         }));
     }
 }
 
-function videoPaused() {
-    if (programPaused) {
-        programPaused = false;
-    } else {
-        lastState = undefined;
-        socket.send(JSON.stringify({
-            "paused": true,
-            "action": "change"
-        }));
-    }
+function videoPlayed(e) {
+    lastState = undefined;
+    socket.send(JSON.stringify({
+        "paused": false,
+        "action": "change"
+    }));
+}
+
+function videoPaused(e) {
+    lastState = undefined;
+    socket.send(JSON.stringify({
+        "paused": true,
+        "action": "change"
+    }));
+}
+
+function videoStopped(e) {
+    lastState = undefined;
+    socket.send(JSON.stringify({
+        "paused": true,
+        "timeStamp": 0,
+        "action": "change"
+    }));
+}
+
+function updateSlider(e) {
+    //vidProgress.value = vid.currentTime / vid.duration * 100;
+    timestampLabel.innerText = timestampToString(vid.currentTime);
 }
 
 function changeSpeed() {
-    txtSpeed.innerText = speed.nodeValue;
+    txtSpeed.innerText = parseFloat(speed.value).toFixed(1);
+    lastState = undefined;
+    socket.send(JSON.stringify({
+        "playSpeed": speed.value,
+        "timeStamp": 0,
+        "action": "change"
+    }));
+}
+
+function inputSpeed() {
+    txtSpeed.innerText = parseFloat(speed.value).toFixed(1);
+}
+
+function changeVolume() {
+    txtVol.innerText = parseFloat(volume.value).toFixed(2);
 }
 
 function fileSelected() {
     vid.src = URL.createObjectURL(file.files[0]);
     vid.oncanplay = () => {
-        updateVideo(lastState);
+        if (lastState) {
+            updateVideo(lastState);
+        }
         vid.oncanplay = undefined;
     };
+}
+
+function timestampToString(time) {
+    var isNeg = time < 0;
+    time = Math.abs(time);
+    var hours = Math.floor(time / 60.0 / 60.0);
+    time -= (hours * 60 * 60);
+    if (hours < 10) {
+        hours = "0" + hours;
+    } else {
+        hours = "" + hours;
+    }
+    var minutes = Math.floor(time / 60.0);
+    time -= (minutes * 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    } else {
+        minutes = "" + minutes;
+    }
+    var seconds = time;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    } else {
+        seconds = "" + seconds;
+    }
+    seconds = seconds.substr(0, 6);
+    return (isNeg ? "-" : "") + hours + ":" + minutes + ":" + seconds;
 }
 
 init();
