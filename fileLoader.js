544c77a29a98074a912888947d6e72cf1a544009
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-04-24 15:03:48 +0200

    Added url text fields

--- fileLoader.js
+++ fileLoader.js
@@ -1,31 +1,49 @@
 const file = document.querySelector("#file");
 const file2 = document.querySelector("#file2");
+const files = document.querySelectorAll(".file");
+const urls = document.querySelectorAll(".url");
+const txtUrl = document.querySelector("#txtUrl");
+const txtUrl2 = document.querySelector("#txtUrl2");
 
 function initFileLoading() {
     file.addEventListener("change", file1Selected);
     file2.addEventListener("change", file2Selected);
+    txtUrl.addEventListener("change", () => {
+        urlSelected(0);
+    });
+    txtUrl2.addEventListener("change", () => {
+        urlSelected(1);
+    });
+}
+
+function fileSelected(id) {
+    vids[id].src = URL.createObjectURL(files[id].files[0]);
+    vids[id].oncanplay = () => {
+        if (lastState) {
+            updateVideo(lastState);
+        }
+        updateSlider();
+        vids[id].oncanplay = undefined;
+    };
+}
+
+function urlSelected(id) {
+    vids[id].src = urls[id].value;
+    vids[id].oncanplay = () => {
+        if (lastState) {
+            updateVideo(lastState);
+        }
+        updateSlider();
+        vids[id].oncanplay = undefined;
+    };
 }
 
 function file1Selected() {
-    vids[0].src = URL.createObjectURL(file.files[0]);
-    vids[0].oncanplay = () => {
-        if (lastState) {
-            updateVideo(lastState);
-        }
-        updateSlider();
-        vids[0].oncanplay = undefined;
-    };
+    fileSelected(0);
 }
 
 function file2Selected() {
-    vids[1].src = URL.createObjectURL(file2.files[0]);
-    vids[1].oncanplay = () => {
-        if (lastState) {
-            updateVideo(lastState);
-        }
-        updateSlider();
-        vids[1].oncanplay = undefined;
-    };
+    fileSelected(1);
 }
 
 initFileLoading();
